e913cf62859b20ecbc9c246024fd578163ae1476
Ingridg <49728531+ingridgamaa96@users.noreply.github.com>
2023-08-16 21:09:45 -0300

    Controller

--- src/main/java/com/projetopicpay/Controllers/UserController.java
+++ src/main/java/com/projetopicpay/Controllers/UserController.java
@@ -6,13 +6,12 @@ import com.projetopicpay.services.UserServices;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RestController("/users")
+@RestController
+@RequestMapping ("/users")
 public class UserController {
 
    @Autowired
@@ -21,7 +20,7 @@ public class UserController {
 
 
     @PostMapping
-    public ResponseEntity<User> createUser (UserDto user){
+    public ResponseEntity<User> createUser (@RequestBody UserDto user){
         User newUser = userServices.createUser(user);
         return new ResponseEntity<>(newUser , HttpStatus.CREATED);
 
